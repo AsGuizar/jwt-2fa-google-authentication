1cf089f0645181438b7eaca3dcac30bb89b5968b
AsGuizar <angelsguizar2004@gmail.com>
2025-02-16 13:36:36 -0600

    Fixed issues, added password confirmation and a welcome page

--- public/script.js
+++ public/script.js
@@ -1,9 +1,15 @@
 async function register() {
     const username = document.getElementById("register-username").value.trim();
     const password = document.getElementById("register-password").value.trim();
+    const confirmPassword = document.getElementById("register-confirm-password").value.trim();
 
-    if (!username || !password) {
-        document.getElementById("message").innerText = "El nombre de usuario y la contraseña no pueden estar vacíos ni contener solo espacios";
+    if (!username || !password || !confirmPassword) {
+        document.getElementById("message").innerText = "El nombre de usuario y las contraseñas no pueden estar vacíos.";
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        document.getElementById("message").innerText = "Las contraseñas no coinciden.";
         return;
     }
 
@@ -17,15 +23,16 @@ async function register() {
     document.getElementById("message").innerText = result.message;
 }
 
+
 async function login() {
-     const username = document.getElementById("login-username").value.trim();
+    const username = document.getElementById("login-username").value.trim();
     const password = document.getElementById("login-password").value.trim();
 
     if (!username || !password) {
         document.getElementById("message").innerText = "El nombre de usuario y la contraseña no pueden estar vacíos ni contener solo espacios";
         return;
     }
-    
+
     const response = await fetch("/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -34,4 +41,8 @@ async function login() {
 
     const result = await response.json();
     document.getElementById("message").innerText = result.message;
+
+    if (result.message === "Inicio de sesión exitoso.") {
+        window.location.href = "../bienvenida.html"; 
+    }
 }
